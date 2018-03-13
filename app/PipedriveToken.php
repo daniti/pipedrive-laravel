54fef6eb8bfd8612dc8d291b63e11c8d34d1ba8f
Daniele Timo <dani@danieletimo.com>
2018-03-13 11:32:38 +0200

    moved token logic from User to PipedriveToken

--- app/PipedriveToken.php
+++ app/PipedriveToken.php
@@ -19,12 +19,44 @@ class PipedriveToken extends Model
 
     public function body()
     {
+        return json_decode($this->decrypt_token());
+    }
+
+    public function store($token)
+    {
+        $salt = $this->token_salt();
+
+        $this->body = $this->encrypt_token($token, $salt);
+        $this->salt = $salt;
+
+        $this->save();
+    }
+
+    protected function token_salt()
+    {
+        return password_hash(env('TOKEN_PASSWORD'), PASSWORD_BCRYPT, ['cost' => 12]);
+    }
+
+    protected function decrypt_token()
+    {
         $key = env('TOKEN_PASSWORD') . $this->salt;
         $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
 
-        $decrypted = openssl_decrypt(base64_decode($this->body), 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
+        return openssl_decrypt(base64_decode($this->body), 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
+    }
 
-        return json_decode($decrypted);
+    protected function encrypt_token($token, $salt)
+    {
+        $body = json_encode($token);
+
+        $password = env('TOKEN_PASSWORD');
+
+        $key = $password . $salt;
+        echo "Key:" . $key . "\n";
+
+        $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
+
+        return base64_encode(openssl_encrypt($body, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv));
     }
 
 }
--- app/User.php
+++ app/User.php
@@ -28,36 +28,11 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    protected function token_salt()
-    {
-        return password_hash(env('TOKEN_PASSWORD'), PASSWORD_BCRYPT, ['cost' => 12]);
-    }
-
-    protected function encrypt_token($token, $salt)
-    {
-        $body = json_encode($token);
-
-        $password = env('TOKEN_PASSWORD');
-
-        $key = $password . $salt;
-        echo "Key:" . $key . "\n";
-
-        $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
-
-        return base64_encode(openssl_encrypt($body, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv));
-    }
-
     public function storeToken($token)
     {
         $pipedrive_token = PipedriveToken::firstOrNew(['user_id' => $this->id]);
+        $pipedrive_token->store($token);
 
-        $salt = $this->token_salt();
-
-        $pipedrive_token->user_id = $this->id;
-        $pipedrive_token->body = $this->encrypt_token($token, $salt);
-        $pipedrive_token->salt = $salt;
-
-        $pipedrive_token->save();
     }
 
     public function pipedrive_token()
